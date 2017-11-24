e3d173e96c18b3b5869ae85468fd3609da328eb8
kanakaraju043 <kanakaraju.gandreddi@gmail.com>
2017-11-24 11:37:23 +0530

    Add files via upload

---
+++ contact.m
@@ -0,0 +1,64 @@
+#import <Contacts/Contacts.h>
+
+@implementation ContactsScan
+
+- (void) contactScan
+{
+    if ([CNContactStore class]) {
+        //ios9 or later
+        CNEntityType entityType = CNEntityTypeContacts;
+        if( [CNContactStore authorizationStatusForEntityType:entityType] == CNAuthorizationStatusNotDetermined)
+         {
+             CNContactStore * contactStore = [[CNContactStore alloc] init];
+             [contactStore requestAccessForEntityType:entityType completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                 if(granted){
+                     [self getAllContact];
+                 }
+             }];
+         }
+        else if( [CNContactStore authorizationStatusForEntityType:entityType]== CNAuthorizationStatusAuthorized)
+        {
+            [self getAllContact];
+        }
+    }
+}
+
+-(void)getAllContact
+{
+    if([CNContactStore class])
+    {
+        //iOS 9 or later
+        NSError* contactError;
+        CNContactStore* addressBook = [[CNContactStore alloc]init];
+        [addressBook containersMatchingPredicate:[CNContainer predicateForContainersWithIdentifiers: @[addressBook.defaultContainerIdentifier]] error:&contactError];
+        NSArray * keysToFetch =@[CNContactEmailAddressesKey, CNContactPhoneNumbersKey, CNContactFamilyNameKey, CNContactGivenNameKey, CNContactPostalAddressesKey];
+        CNContactFetchRequest * request = [[CNContactFetchRequest alloc]initWithKeysToFetch:keysToFetch];
+        BOOL success = [addressBook enumerateContactsWithFetchRequest:request error:&contactError usingBlock:^(CNContact * __nonnull contact, BOOL * __nonnull stop){
+            [self parseContactWithContact:contact];
+        }];
+    }
+}
+
+- (void)parseContactWithContact :(CNContact* )contact
+{
+    NSString * firstName =  contact.givenName;
+    NSString * lastName =  contact.familyName;
+    NSString * phone = [[contact.phoneNumbers valueForKey:@"value"] valueForKey:@"digits"];
+    NSString * email = [contact.emailAddresses valueForKey:@"value"];
+    NSArray * addrArr = [self parseAddressWithContac:contact];
+}
+
+- (NSMutableArray *)parseAddressWithContac: (CNContact *)contact
+{
+    NSMutableArray * addrArr = [[NSMutableArray alloc]init];
+    CNPostalAddressFormatter * formatter = [[CNPostalAddressFormatter alloc]init];
+    NSArray * addresses = (NSArray*)[contact.postalAddresses valueForKey:@"value"];
+    if (addresses.count > 0) {
+        for (CNPostalAddress* address in addresses) {
+            [addrArr addObject:[formatter stringFromPostalAddress:address]];
+        }
+    }
+    return addrArr;
+}
+
+@end
